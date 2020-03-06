da817f75ada20d79d6890fcc3ead48752e11d01f
Junjun Zhao <junjun.zhao@oriente.com>
2020-03-06 11:18:32 +0800

    day1

---
+++ src/day1/Solution.java
@@ -0,0 +1,25 @@
+package day1;
+
+public class Solution {
+
+    public static int removeDuplicates(int[] n) {
+
+        if (n.length == 0) {
+            return 0;
+        }
+
+        int i = 0;
+        for (int j = 1; j < n.length; j++) {
+            if (n[j] != n[i]) {
+                i++;
+                n[i] = n[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] bb=new int[]{1,6,2,2,3};
+        System.out.println( removeDuplicates(bb));
+    }
+}
